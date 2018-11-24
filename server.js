7634fadae13f717f7eb6a096742939a1e99b275a
chinmoy96 <chinmoy.pradhan4@gmail.com>
2018-11-24 23:11:32 +0530

    signin signup apis added

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const SignUp=require('./server/APIs/SignUp')
+var mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost:27017/lex').then(()=>{
+  console.log("Database connected"); 
+});
+
 app.use('/',SignUp);
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- server/APIs/SignUp.js
+++ server/APIs/SignUp.js
@@ -1,7 +1,77 @@
-var express=require('express');
-var route=express.Router();
+const express=require('express');
+const route=express.Router();
+const User=require('./../Models/User');
 
-route.get('/api/hello', (req, res) => {
-        res.send("hello");
+route.post('/api/signup',(req,res)=>{
+    let {name,password} =req.body;
+    name=name.toLowerCase();
+    var userObj=new User();
+    userObj.name=name;
+    userObj.password=password;
+    userObj.save((err)=>{
+        if(err){
+            res.send({
+                success:false,
+                message:"server error"
+            });
+        }
+        else{
+            res.send({
+                success:true,
+                message:"new user added"
+            });
+        }
+    })
+})
+
+route.post('/api/signin',(req,res)=>{
+    let {name,password} =req.body;
+    name=name.toLowerCase();
+    var userObj=new User();
+    User.find({name:name},(err,users)=>{
+        if(err){
+            res.send({
+                success:false,
+                message:"server error"
+            });
+        }
+        console.log(users);
+        if(users.length==0){
+            res.send({
+                success:false,
+                message:"No users found"
+            });
+        }
+        else
+        {
+            if(users[0].password!=password){
+                res.send({
+                    success:false,
+                    message:"password does not match"
+                });
+            }
+            res.send({
+                success:true,
+                message:"log in successful"
+            });
+        }
+    })
+})
+
+route.get('/api/users', (req, res) => {
+        User.find((err,users)=>{
+            if(err){
+                res.send({
+                    success:false,
+                    message:"server error"
+                });
+            }
+            else{
+                res.send({
+                    success:true,
+                    users:users
+                });
+            }
+        })
   });
 module.exports=route;
