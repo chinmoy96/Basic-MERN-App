5b11af235f786c0dbd0527df05b6082486c88da5
chinmoy96 <chinmoy.pradhan4@gmail.com>
2018-11-25 18:17:28 +0530

    config file added

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const SignUp=require('./server/APIs/SignUp')
 var mongoose = require('mongoose');
-
+const DB_Path=require('./config/config').con_string;
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect('mongodb://localhost:27017/lex').then(()=>{
+mongoose.connect(DB_Path).then(()=>{
   console.log("Database connected"); 
 });
 
